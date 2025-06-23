fe9935ce28129e806d25de2f3c46ab8e02bbee7d
tu-nombre-de-usuario <braynermc2021@gmail.com>
2025-06-22 19:25:05 -0500

    prueba ec2

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 import boto3
-import json
 import os
 
 def main():
-    bucket_name = 's3-data-salida-covid-brayner573'
-    s3_key = 'data_transformada.json'
-    local_file = 'data_transformada.json'
-
-    # 🔧 Simulación: crea el archivo local
-    data = {
-        "mensaje": "Archivo generado correctamente",
-        "reglas_aplicadas": 20,
-        "estado": "completado"
-    }
-    with open(local_file, 'w') as f:
-        json.dump(data, f)
-
-    # 🔐 Cliente S3
     s3 = boto3.client('s3')
 
-    # 📤 Sube el archivo al bucket de salida
-    s3.upload_file(local_file, bucket_name, s3_key)
-    print(f"✅ Archivo '{local_file}' subido a S3 en '{bucket_name}/{s3_key}'")
+    bucket_name = 's3-data-salida-covid-brayner573'
+    local_file = 'data_transformada.json'
+    s3_key = 'data_transformada.json'
+
+    # Verificar si ya existe
+    try:
+        s3.head_object(Bucket=bucket_name, Key=s3_key)
+        print("🟢 El archivo ya existe en S3. No se subirá de nuevo.")
+    except:
+        print("🔵 El archivo no existe en S3. Subiendo...")
+        s3.upload_file(local_file, bucket_name, s3_key)
+        print("✅ Subido correctamente.")
 
 if __name__ == '__main__':
     main()
